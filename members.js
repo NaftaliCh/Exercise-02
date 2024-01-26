7506506b48ab74df28c93a17c87fd4ab28c4fc0d
NaftaliCh <127361586+NaftaliCh@users.noreply.github.com>
2024-01-26 14:48:04 +0800

    perbaikan

--- members.js
+++ members.js
@@ -1,6 +1,6 @@
 const members = [
     { id: 'S11810219', nim: '105021810053', nama: 'Naftali Maleke', nomor_telp: '082863639624' },
-    // Add more members as needed
+   
   ];
   
   module.exports = {
