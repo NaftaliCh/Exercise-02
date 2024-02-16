2a0a470c6e956c4edd9960a58a3460c5f86c7f45
NaftaliCh <127361586+NaftaliCh@users.noreply.github.com>
2024-02-16 12:40:32 +0800

    Delete members.js

--- members.js
+++
@@ -1,8 +0,0 @@
-const members = [
-    { id: 'S11810219', nim: '105021810053', nama: 'Naftali Maleke', nomor_telp: '082863639624' },
-    
-  ];
-  
-  module.exports = {
-    getMembers: () => members,
-  };
